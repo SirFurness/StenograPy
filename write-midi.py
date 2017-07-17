c0302491b4cb7c4316016ebc10b34b88bffdfad8
thomas302 <mr.actually12@gmail.com>
2017-07-16 20:54:59 -0700

    Update write-midi.py

--- write-midi.py
+++ write-midi.py
@@ -1,1 +1,10 @@
 from music21 import *
+def write(filename):
+  
+     me = MidiEvent(mt)
+     me.type = "NOTE_ON"
+     me.channel = 1
+     me.time = None #d
+     me.pitch = p
+     me.velocity = v
+     mt.events.append(me)
