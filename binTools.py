a20741b64594c90e0affac0d8aa0638a088fcfd4
thomas302 <mr.actually12@gmail.com>
2018-06-26 23:02:24 -0700

    Update and rename binTools to binTools.py

---
+++ binTools.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 26 13:49:21 2018
+@author: Joel Velez
+"""
+class binTools:
+  #convert string to hex
+  def strToBin(s):
+    a_bytes = bytes(s, "ascii")
+    b = ' '.join(["{0:b}".format(x) for x in a_bytes])
+    return b
+  
