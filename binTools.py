f971a0f5eee57ea992187e117316c0325580b015
thomas302 <mr.actually12@gmail.com>
2018-06-26 23:10:02 -0700

    Update binTools.py

--- binTools.py
+++ binTools.py
@@ -4,7 +4,7 @@ Created on Tue Jun 26 13:49:21 2018
 @author: Joel Velez
 """
 class binTools:
-  #convert string to hex
+  #convert string to binary
   def strToBin(self, s):
     a_bytes = bytes(s, "ascii")
     b = ' '.join(["{0:b}".format(x) for x in a_bytes])
