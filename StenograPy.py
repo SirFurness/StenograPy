ca830869a910ed53ec7c71431db9867ab415335c
SirFurness <eric.1334@icloud.com>
2018-06-26 23:13:41 -0700

    Lay out plan with functions

--- StenograPy.py
+++ StenograPy.py
@@ -3,6 +3,17 @@
 Created on Tue Jun 26 13:49:21 2018
 @author: Joel Velez and Eric Paul
 """
-class writeData:
-  def __init__(self):
-    
+
+def getInputString():
+  pass
+
+def convertStringToBinary():
+  pass
+
+def encodeFile():
+  pass
+
+if __name__ == "__main__":
+  input = getInputString()
+  binary = convertStringToBinary()
+  encodeFile(binary)
